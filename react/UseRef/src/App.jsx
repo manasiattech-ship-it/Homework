6b0ceab5e2bfcdbccca6807feb348fb69de72278
Manasi Mahajan <manasiattech@gmail.com>
2026-07-15 13:45:25 -0400

    uref_no rerendering of the form

---
+++ react/UseRef/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from './components/Form'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-300 p-5'>
+        <Form/>
+    </div>
+  )
+}
+
+export default App
---
+++ react/UseRef/src/components/Form.jsx
@@ -0,0 +1,37 @@
+import React, { useRef, useState } from 'react'
+
+const Form = () => {
+    const [formData, setFormData] = useState({});
+    let inpRef = useRef({})
+
+    let handleSub = (e) => {
+        e.preventDefault()
+        console.log("in submit")
+          console.log(inpRef.current.prodName.value)
+          console.log(inpRef.current.image.value)
+          console.log(inpRef.current.category.value)
+          console.log(inpRef.current.price.value)
+
+    }
+  return (
+    <div className='w-80 h-screen'>
+ 
+        <form className='flex flex-col gap-4 p-6 rounded  bg-white' onSubmit={handleSub}>
+            <input ref={(e) => inpRef.current.prodName = e} type="text" className="p-2 border-gray-400-rounded"  name="prodname" placeholder='product name'></input>
+            <input ref={(e) => inpRef.current.price = e}  type="text"  className="p-2 border-gray-400-rounded"  placeholder='Price'></input>
+            <span className="p-2 border-gray-400-rounded" >Select Category</span><select  ref={(e) => inpRef.current.category = e}  className="p-2 border-gray-400-rounded" >
+                <option value="Mens">Mens</option>            
+                <option value="women">Women</option>            
+                <option value="kids">kids</option>            
+                
+                </select>
+
+                <input  ref={(e) => inpRef.current.image = e}  type="text" placeholder='image'></input>
+                <button className='p-2 bg-blue-600 text-white rounded'>Create</button>
+            </form>
+      
+    </div>
+  )
+}
+
+export default Form

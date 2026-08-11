ea0587098db7a6e63d38baf622c5388079be1435
Manasi Mahajan <manasiattech@gmail.com>
2026-08-11 16:49:33 -0400

    auth page

--- CLASS/react/day15Auth/src/routes/ProtectedRoute.jsx
+++ CLASS/react/day15Auth/src/routes/ProtectedRoute.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navigate, Outlet } from 'react-router'
+import { Auth } from '../context/AuthContext';
 
 const ProtectedRoute = () => {
     const {loggedInUser} = useContext(Auth);
